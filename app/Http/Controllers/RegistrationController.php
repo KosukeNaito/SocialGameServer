0d69361b384a7d697aed7694954e01751900821b
KosukeNaito <tonightoo7110@gmail.com>
2021-05-09 03:05:49 +0900

    RegisterController add start Method

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
 {
-    //
+    public function Start(Request $request)
+    {
+        $user_id = $request->user_id;
+
+        //クライアントに返すレスポンスを入れる
+        $response = array();
+
+        return $response;
+
+    }
 }
