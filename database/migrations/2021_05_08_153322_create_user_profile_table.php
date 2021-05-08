6f3dbe66db20b1169b9bff65371e8a55d28cb8d6
KosukeNaito <tonightoo7110@gmail.com>
2021-05-09 01:30:52 +0900

    migration create edit

--- database/migrations/2021_05_08_153322_create_user_profile_table.php
+++ database/migrations/2021_05_08_153322_create_user_profile_table.php
@@ -14,18 +14,11 @@ class CreateUserProfileTable extends Migration
     public function up()
     {
         Schema::create('user_profile', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->string('user_id', 37);
+            $table->string('user_name', 32);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->primary('user_id');
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('user_profile');
     }
 }
